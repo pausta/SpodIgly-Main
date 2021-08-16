a96deef412b6e2cba7651336a7c1fe9eed596ea4
pausta <pausta1995@gmail.com>
2021-08-16 21:35:33 +0200

    Created VS project

---
+++ SpodIgly-Main.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SpodIgly_Main.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
