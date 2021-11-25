8b3339fca35d8e1765490a7fe8725ebd1d87320e
pausta <pausta1995@gmail.com>
2021-11-25 22:21:45 +0100

    Struktura bazy danych / Entity Framework /  web.config /
    Tworzenie struktury bazy danych DAL,
    Pobranie Enitity Framework,
    Stworzenie Connection String

---
+++ SpodIgly-Main/DAL/StoreContext.cs
@@ -0,0 +1,21 @@
+﻿using SpodIgly_Main.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SpodIgly_Main.DAL
+{
+    // w tym momencie dodajemy references w Odwolania - NuGet i pobieramy paczkę EntityFramework z zależnościami
+    public class StoreContext : DbContext    // Storecontext -  reprezentuje całą bazę danych
+    {  
+        // w tej klasie chcemy mieć trzy tabelki:
+        public DbSet<Album> Albums { get; set; }  // DbSet  - pojedyncze tabelki tej bazy danych
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+
+    }
+}
